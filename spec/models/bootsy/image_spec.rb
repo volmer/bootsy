051c93c4fbccdabab2a7acca9ce2a6d13f0b53fd
Volmer Soares <lrvolmer@gmail.com>
2012-07-23 16:57:38 -0300

    Updated specs

--- spec/models/bootsy/image_spec.rb
+++ spec/models/bootsy/image_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
 describe Bootsy::Image do
-  before :each do
-    @image = FactoryGirl.build :image
+  let :image do
+    FactoryGirl.build :image
   end
 
   it 'is valid with proper values' do
-    @image.should be_valid
+    image.should be_valid
   end
 
   it 'is not valid without a gallery' do
-    @image.image_gallery = nil
-    @image.should_not be_valid
+    image.image_gallery = nil
+    image.should_not be_valid
   end
 
   it 'is not valid without an image file' do
-    @image.remove_image_file!
-    @image.should_not be_valid
+    image.remove_image_file!
+    image.should_not be_valid
   end
 end
