0c009ee5d60348f8c47a48800440ec5df8fb5aa1
volmer <volmerius@gmail.com>
2012-09-17 10:53:56 -0300

    Fixed class addition problem

--- spec/lib/form_helper_spec.rb
+++ spec/lib/form_helper_spec.rb
@@ -20,8 +20,8 @@ describe Bootsy::FormHelper do
       @resource.stub(:content)
     end
 
-    it 'renders a text_area with the HTML class \'bootsy_text_area\'' do
-      dummy_object.should_receive(:text_area).with(anything, :content, hash_including(:class => 'bootsy_text_area')).and_return('<textarea>')
+    it 'renders a text_area with the HTML class :bootsy_text_area' do
+      dummy_object.should_receive(:text_area).with(anything, :content, hash_including(:class => [:bootsy_text_area])).and_return('<textarea>')
       dummy_object.bootsy_area(@resource, :content).should include('<textarea>')
     end
 
@@ -34,13 +34,18 @@ describe Bootsy::FormHelper do
     end
 
     it 'passes the options hash to the text_area' do
-      dummy_object.should_receive(:text_area).with(anything, :content, hash_including(:class => 'bootsy_text_area', op1: '1', op2: '2')).and_return('<textarea>')
+      dummy_object.should_receive(:text_area).with(anything, :content, hash_including(:class => [:bootsy_text_area], op1: '1', op2: '2')).and_return('<textarea>')
       dummy_object.bootsy_area(@resource, :content, {op1: '1', op2: '2'}).should include('<textarea>')
     end
 
-    it 'appends custom HTML classes on the text_area' do
-      dummy_object.should_receive(:text_area).with(anything, :content, hash_including(:class => 'my_html_class bootsy_text_area')).and_return('<textarea>')
+    it 'appends custom HTML classes passed as string on the text_area' do
+      dummy_object.should_receive(:text_area).with(anything, :content, hash_including(:class => ['my_html_class', :bootsy_text_area])).and_return('<textarea>')
       dummy_object.bootsy_area(@resource, :content, {:class => 'my_html_class', op1: '1', op2: '2'}).should include('<textarea>')
+    end
+
+    it 'appends custom HTML classes passed as array on the text_area' do
+      dummy_object.should_receive(:text_area).with(anything, :content, hash_including(:class => ['my_html_class', :bootsy_text_area])).and_return('<textarea>')
+      dummy_object.bootsy_area(@resource, :content, {:class => ['my_html_class'], op1: '1', op2: '2'}).should include('<textarea>')
     end
 
     context 'when a non-media_container is passed' do
