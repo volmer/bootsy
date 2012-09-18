fffa370c519c88c64fe46b0dbd640866565142cb
volmer <volmerius@gmail.com>
2012-09-18 15:11:28 -0300

    Editor options

--- lib/bootsy/form_helper.rb
+++ lib/bootsy/form_helper.rb
@@ -2,32 +2,46 @@ module Bootsy
   module FormHelper
     def bootsy_area object, method, options = {}
 
-      resource = options.delete :resource
+      container = options.delete :container
 
-      unless resource.kind_of?(MediaContainer) || (resource.nil? && object.kind_of?(MediaContainer))
-        raise ArgumentError, 'Bootsy area needs a model or a resource as its option'
+      unless container.kind_of?(MediaContainer) || (container.nil? && object.kind_of?(MediaContainer))
+        raise ArgumentError, 'Bootsy area needs a model or a container as its option'
       end
 
-      bootsy_options = options.delete :bootsy_options
+      editor_options = options.delete :editor_options
 
-      unless bootsy_options.nil?
-        if bootsy_options[:uploader] == false
-          options[:'data-enable-uploader'] = 'false'
-        end
+      unless editor_options.nil?
+        
+        options[:'data-enable-uploader'] = 'false' if editor_options[:uploader] == false
 
-        if bootsy_options[:alert_unsaved] == false
-          options[:'data-alert-unsaved'] = 'false'
-        end
+        options[:'data-alert-unsaved'] = 'false' if editor_options[:alert_unsaved] == false
+      
+        options[:'data-enable-font-styles'] = 'false' if editor_options[:font_styles] == false
+      
+        options[:'data-enable-emphasis'] = 'false' if editor_options[:emphasis] == false
+      
+        options[:'data-enable-lists'] = 'false' if editor_options[:lists] == false
+      
+        options[:'data-enable-html'] = 'true' if editor_options[:html] == true
+      
+        options[:'data-enable-link'] = 'false' if editor_options[:link] == false
+      
+        options[:'data-enable-image'] = 'false' if editor_options[:image] == false
+      
+        options[:'data-enable-color'] = 'false' if editor_options[:color] == false
+        
       end
 
       object_name = object.class.name.underscore
       
-      output = self.render 'bootsy/images/modal', {resource: resource || object}
+      output = self.render 'bootsy/images/modal', {container: container || object}
       options[:class] = (options[:class].nil? ? [] : (options[:class].kind_of?(Array) ? options[:class] : [options[:class]])) + [:bootsy_text_area]
       output += self.text_area object_name, method, options
-      if resource.nil? || (resource == object)
+
+      if container.nil? || (container == object)
         output += self.hidden_field object_name, :bootsy_image_gallery_id, :class => 'bootsy_image_gallery_id'
       end
+      
       output
     end
   end
--- spec/lib/form_helper_spec.rb
+++ spec/lib/form_helper_spec.rb
@@ -13,69 +13,116 @@ describe Bootsy::FormHelper do
 
   describe '#bootsy_area' do
     before :each do
-      dummy_object.stub(:render) {|template, variables| variables[:resource].to_s }
+      dummy_object.stub(:render) {|template, variables| variables[:container].to_s }
       dummy_object.stub(:text_area).and_return('<textarea>')
       dummy_object.stub(:hidden_field).and_return('<hidden>')
-      @resource = double('resource').extend(Bootsy::MediaContainer)
-      @resource.stub(:content)
+      @container = double('container').extend(Bootsy::MediaContainer)
+      @container.stub(:content)
     end
 
     it 'renders a text_area with the HTML class :bootsy_text_area' do
       dummy_object.should_receive(:text_area).with(anything, :content, hash_including(:class => [:bootsy_text_area])).and_return('<textarea>')
-      dummy_object.bootsy_area(@resource, :content).should include('<textarea>')
+      dummy_object.bootsy_area(@container, :content).should include('<textarea>')
     end
 
     it 'renders a hidden_field' do
-      dummy_object.bootsy_area(@resource, :content).should include('<hidden>')
+      dummy_object.bootsy_area(@container, :content).should include('<hidden>')
     end
 
-    it 'renders the gallery of the resource' do
-      dummy_object.bootsy_area(@resource, :content).should include(@resource.to_s)
+    it 'renders the gallery of the container' do
+      dummy_object.bootsy_area(@container, :content).should include(@container.to_s)
     end
 
     it 'passes the options hash to the text_area' do
       dummy_object.should_receive(:text_area).with(anything, :content, hash_including(:class => [:bootsy_text_area], op1: '1', op2: '2')).and_return('<textarea>')
-      dummy_object.bootsy_area(@resource, :content, {op1: '1', op2: '2'}).should include('<textarea>')
+      dummy_object.bootsy_area(@container, :content, {op1: '1', op2: '2'}).should include('<textarea>')
     end
 
     it 'appends custom HTML classes passed as string on the text_area' do
       dummy_object.should_receive(:text_area).with(anything, :content, hash_including(:class => ['my_html_class', :bootsy_text_area])).and_return('<textarea>')
-      dummy_object.bootsy_area(@resource, :content, {:class => 'my_html_class', op1: '1', op2: '2'}).should include('<textarea>')
+      dummy_object.bootsy_area(@container, :content, {:class => 'my_html_class', op1: '1', op2: '2'}).should include('<textarea>')
     end
 
     it 'appends custom HTML classes passed as array on the text_area' do
       dummy_object.should_receive(:text_area).with(anything, :content, hash_including(:class => ['my_html_class', :bootsy_text_area])).and_return('<textarea>')
-      dummy_object.bootsy_area(@resource, :content, {:class => ['my_html_class'], op1: '1', op2: '2'}).should include('<textarea>')
+      dummy_object.bootsy_area(@container, :content, {:class => ['my_html_class'], op1: '1', op2: '2'}).should include('<textarea>')
     end
 
     context 'when a non-media_container is passed' do
       it 'raises an error' do
         expect{
-          dummy_object.bootsy_area(double('resource'), :content)
+          dummy_object.bootsy_area(double('container'), :content)
         }.to raise_error(ArgumentError)
       end
     end
 
-    context 'when a specific resource is passed' do
+    context 'when a specific container is passed' do
 
       before :each do
         @specific = double('specific').extend(Bootsy::MediaContainer)
       end
 
-      it 'renders the gallery of the specific resource' do
-        dummy_object.bootsy_area(@resource, :content, {resource: @specific}).should include(@specific.to_s)
+      it 'renders the gallery of the specific container' do
+        dummy_object.bootsy_area(@container, :content, {container: @specific}).should include(@specific.to_s)
       end
 
       it 'does not render a hidden_field' do
-        dummy_object.bootsy_area(@resource, :content, {resource: @specific}).should_not include('<hidden>')
+        dummy_object.bootsy_area(@container, :content, {container: @specific}).should_not include('<hidden>')
       end
 
-      context 'when a non-media_container specific resource is passed' do
+      context 'when a non-media_container specific container is passed' do
         it 'raises an error' do
           expect{
-            dummy_object.bootsy_area(@resource, :content, {resource: double('other_specific')})
+            dummy_object.bootsy_area(@container, :content, {container: double('other_specific')})
           }.to raise_error(ArgumentError)
         end
+      end
+    end
+
+    context 'when editor_options is passed' do
+      it "adds data-enable-uploader='false' for uploader: false" do
+        dummy_object.should_receive(:text_area).with(anything, anything, hash_including(:'data-enable-uploader' => 'false'))
+        dummy_object.bootsy_area(@container, :content, editor_options: {uploader: false})
+      end
+
+      it "adds data-alert-unsaved='false' for alert_unsaved: false" do
+        dummy_object.should_receive(:text_area).with(anything, anything, hash_including(:'data-alert-unsaved' => 'false'))
+        dummy_object.bootsy_area(@container, :content, editor_options: {alert_unsaved: false})
+      end
+
+      it "adds data-enable-font-styles='false' for font_styles: false" do
+        dummy_object.should_receive(:text_area).with(anything, anything, hash_including(:'data-enable-font-styles' => 'false'))
+        dummy_object.bootsy_area(@container, :content, editor_options: {font_styles: false})
+      end
+
+      it "adds data-enable-emphasis='false' for emphasis: false" do
+        dummy_object.should_receive(:text_area).with(anything, anything, hash_including(:'data-enable-emphasis' => 'false'))
+        dummy_object.bootsy_area(@container, :content, editor_options: {emphasis: false})
+      end
+
+      it "adds data-enable-lists='false' for lists: false" do
+        dummy_object.should_receive(:text_area).with(anything, anything, hash_including(:'data-enable-lists' => 'false'))
+        dummy_object.bootsy_area(@container, :content, editor_options: {lists: false})
+      end
+
+      it "adds data-enable-html='true' for html: true" do
+        dummy_object.should_receive(:text_area).with(anything, anything, hash_including(:'data-enable-html' => 'true'))
+        dummy_object.bootsy_area(@container, :content, editor_options: {html: true})
+      end
+
+      it "adds data-enable-link='false' for link: false" do
+        dummy_object.should_receive(:text_area).with(anything, anything, hash_including(:'data-enable-link' => 'false'))
+        dummy_object.bootsy_area(@container, :content, editor_options: {link: false})
+      end
+
+      it "adds data-enable-image='false' for image: false" do
+        dummy_object.should_receive(:text_area).with(anything, anything, hash_including(:'data-enable-image' => 'false'))
+        dummy_object.bootsy_area(@container, :content, editor_options: {image: false})
+      end
+
+      it "adds data-enable-color='false' for color: false" do
+        dummy_object.should_receive(:text_area).with(anything, anything, hash_including(:'data-enable-color' => 'false'))
+        dummy_object.bootsy_area(@container, :content, editor_options: {color: false})
       end
     end
   end
