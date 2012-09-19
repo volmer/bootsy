b45bb19792aab26d9203966d97c87ae094759c94
volmer <volmerius@gmail.com>
2012-09-19 14:28:42 -0300

    Updated uploader specs

--- spec/uploaders/image_uploader_spec.rb
+++ spec/uploaders/image_uploader_spec.rb
@@ -15,8 +15,8 @@ describe Bootsy::ImageUploader do
   end
 
   context 'the original version' do
-    it "should limit 940 by 940 pixels" do
-      @uploader.thumb.should be_no_larger_than(940, 940)
+    it "should limit 1160 by 2000 pixels" do
+      @uploader.should be_no_larger_than(1160, 2000)
     end
   end
 
@@ -27,20 +27,20 @@ describe Bootsy::ImageUploader do
   end
 
   context 'the small version' do
-    it "should limit 160 by 160 pixels" do
-      @uploader.small.should be_no_larger_than(160, 160)
+    it "should limit to dimensions defined at Bootsy.small_image" do
+      @uploader.small.should be_no_larger_than(Bootsy.small_image[:width], Bootsy.small_image[:height])
     end
   end
 
   context 'the medium version' do
-    it "should limit 300 by 300 pixels" do
-      @uploader.thumb.should be_no_larger_than(300, 300)
+    it "should limit to dimensions defined at Bootsy.medium_image" do
+      @uploader.thumb.should be_no_larger_than(Bootsy.medium_image[:width], Bootsy.medium_image[:height])
     end
   end
 
   context 'the large version' do
-    it "should limit 600 by 600 pixels" do
-      @uploader.thumb.should be_no_larger_than(600, 600)
+    it "should limit to dimensions defined at Bootsy.large_image" do
+      @uploader.thumb.should be_no_larger_than(Bootsy.large_image[:width], Bootsy.large_image[:height])
     end
   end
 end
