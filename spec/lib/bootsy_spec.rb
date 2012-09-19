38bd8f37e6808eca93c00e2c1a551488d3b553f2
volmer <volmerius@gmail.com>
2012-09-19 14:58:48 -0300

    Updated bootsy spec

--- spec/lib/bootsy_spec.rb
+++ spec/lib/bootsy_spec.rb
@@ -1,11 +1,30 @@
 require 'spec_helper'
 
 describe Bootsy do
+  subject { Bootsy }
+
   describe '.setup' do
     it 'yields self' do
-      Bootsy.setup do |config|
-        config.should == Bootsy
+      subject.setup do |config|
+        config.should == subject
       end
     end
+  end
+
+  describe 'default values' do
+    its(:image_versions_available) { should == [:small, :medium, :large, :original] }
+
+    its(:allow_destroy) { should == true }
+
+    its(:small_image) { should == {width: 160, height: 160} }
+
+    its(:medium_image) { should == {width: 360, height: 360} }
+
+    its(:large_image) { should == {width: 760, height: 760} }
+
+    its(:original_image) { should == {} }
+
+    its(:store_dir) { should == 'uploads' }
+
   end
 end
