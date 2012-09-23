a386a2ba15caeb545dd793d807aa27b496785638
volmer <lrvolmer@gmail.com>
2012-09-22 22:17:53 -0300

    Dismiss alert before running features

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,5 +1,10 @@
 Before do
   DatabaseCleaner.start
+
+  begin
+    page.driver.browser.switch_to.alert.accept
+  rescue Selenium::WebDriver::Error::NoAlertOpenError
+  end
 end
 
 After do |scenario|
