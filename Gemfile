b3870ab1361589877d6c31727d9245f0caf3db13
Volmer Soares <volmerius@gmail.com>
2013-06-08 21:11:40 -0300

    Update Rails version to 4.0.0-rc1.

--- Gemfile
+++ Gemfile
@@ -3,8 +3,8 @@ source "http://rubygems.org"
 gemspec
 
 # Gems used by the dummy application
-gem 'rails', '~> 3.2'
-gem 'jquery-rails', '~> 2.2'
+gem 'rails', '~> 4.0.0.rc1'
+gem 'jquery-rails', '~> 3.0'
 gem 'twitter-bootstrap-rails', '~> 2.2'
 gem 'sqlite3', '~> 1.3'
 
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -6,9 +6,8 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
-Bundler.require(*Rails.groups(assets: %w(development test)))
+Bundler.require(:default, Rails.env)
 
 require 'bootsy'
 
