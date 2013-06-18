e993d939c8d1b5b24ba6a0e0b63c11f1a7a075fc
Volmer Soares <volmerius@gmail.com>
2013-06-17 21:24:56 -0300

    Add scope method to destroy orphan image galleries.

--- lib/bootsy/activerecord/image_gallery.rb
+++ lib/bootsy/activerecord/image_gallery.rb
@@ -2,5 +2,7 @@ module Bootsy
   class ImageGallery < ActiveRecord::Base
     belongs_to :bootsy_resource, polymorphic: true
     has_many :images, dependent: :destroy
+
+    scope :destroy_orphans, ->(time_limit) { where('created_at < ? AND bootsy_resource_id IS NULL', time_limit).destroy_all }
   end
 end
