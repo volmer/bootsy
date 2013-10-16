bbdfefabd7e2e29b2c615664cd135053450e6a2b
Volmer Soares <volmerius@gmail.com>
2013-10-15 22:28:32 -0300

    Fix expectations in step definitions.

--- features/step_definitions/image_gallery_steps.rb
+++ features/step_definitions/image_gallery_steps.rb
@@ -5,10 +5,10 @@ Given 'there is a post with images' do
 end
 
 Then 'I should see the image gallery' do
-  page.should have_css('div#bootsy_image_gallery', visible: true)
+  page.should have_css('#bootsy_image_gallery', visible: true)
 end
 
 Then /^the image gallery should( not)? be empty$/ do |negate|
   expectation = negate ? :should : :should_not
-  page.send expectation, have_css('div#bootsy_image_gallery div.modal-body ul.thumbnails a.thumbnail', visible: true)
+  page.send expectation, have_css('#bootsy_image_gallery .bootsy-image', visible: true)
 end
