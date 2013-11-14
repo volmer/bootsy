aad11f40a572716f3f6ffe251d76cb08fed76998
Volmer Soares <volmerius@gmail.com>
2013-11-14 14:28:15 -0200

    Replace twitter-bootstrap-rails with bootstrap-sass on the dummy app.

--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -12,6 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
+//= require bootstrap
 //= require bootsy
 //= require_tree .
--- spec/dummy/app/assets/javascripts/bootstrap.js
+++
@@ -1,5 +0,0 @@
-jQuery(function() {
-  $("a[rel=popover]").popover();
-  $(".tooltip").tooltip();
-  $("a[rel=tooltip]").tooltip();
-});
