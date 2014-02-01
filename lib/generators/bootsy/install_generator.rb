6e1d8737b3c78b7d0d20278b728527eaf4863feb
Volmer Soares <volmerius@gmail.com>
2014-02-01 11:15:18 -0200

    Warn when the assets manifest is not found.

--- lib/generators/bootsy/install_generator.rb
+++ lib/generators/bootsy/install_generator.rb
@@ -4,37 +4,47 @@ module Bootsy
       source_root __FILE__
 
       def add_routes
-        route "mount Bootsy::Engine => '/bootsy', as: 'bootsy'"
+        route("mount Bootsy::Engine => '/bootsy', as: 'bootsy'")
       end
 
       def copy_locale
-        copy_file "../../../../config/locales/en.yml", "config/locales/bootsy.en.yml"
+        copy_file('../../../../config/locales/en.yml', 'config/locales/bootsy.en.yml')
       end
 
       def add_assets
-
-        [{original: 'app/assets/javascripts/application.js',
-          skip_if: 'require bootsy',
-          content: "\n//= require bootsy",
-          position: {after: '//= require jquery_ujs'}},
-         {original: 'app/assets/stylesheets/application.css',
-          skip_if: 'require bootsy',
-          content: "\n *= require bootsy",
-          position: {after: '*= require_self'}}]. each do |params|
-
-          if File.binread(params[:original]).include?(params[:skip_if])
-            say_status 'skipped', "insert into #{params[:original]}", :yellow
+        [
+          {
+            original: 'app/assets/javascripts/application.js',
+            skip_if: 'require bootsy',
+            content: "\n//= require bootsy",
+            position: {
+              after: '//= require jquery_ujs'
+            }
+          },
+          {
+            original: 'app/assets/stylesheets/application.css',
+            skip_if: 'require bootsy',
+            content: "\n *= require bootsy",
+            position: {
+              after: '*= require_self'
+            }
+          }
+        ].each do |params|
+          if File.exists?(params[:original])
+            if File.binread(params[:original]).include?(params[:skip_if])
+              say_status('skipped', "insert into #{params[:original]}", :yellow)
+            else
+              insert_into_file(params[:original], params[:content], params[:position])
+            end
           else
-            insert_into_file params[:original], params[:content], params[:position]
+            say_status('not found', "#{params[:original]} not found. You must manually require Bootsy in your assets pipeline.", :red)
           end
         end
-
       end
 
       def copy_config
-        template '../templates/bootsy.rb', 'config/initializers/bootsy.rb'
+        template('../templates/bootsy.rb', 'config/initializers/bootsy.rb')
       end
-
     end
   end
 end
