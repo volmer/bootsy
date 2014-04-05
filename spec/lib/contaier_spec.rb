a75925bd9c780f2d0471380cbb17e65e55949535
Volmer Soares <volmerius@gmail.com>
2014-04-05 16:26:01 -0300

    Spec example to ensure that an image gallery is not overwritten.

--- spec/lib/contaier_spec.rb
+++ spec/lib/contaier_spec.rb
@@ -28,15 +28,22 @@ describe Bootsy::Container do
     it 'returns nil if not present' do
       expect(post.bootsy_image_gallery_id).to be_nil
     end
+  end
+
+  describe '#bootsy_image_gallery_id=' do
+    let(:image_gallery) { FactoryGirl.create(:image_gallery) }
 
     it 'sets an image gallery if container does not have one yet' do
-      post = Post.new title: 'Test', content: 'Test'
-      image_gallery = FactoryGirl.create(:image_gallery)
       post.bootsy_image_gallery_id = image_gallery.id
-      post.save!
 
       expect(post.bootsy_image_gallery).to eq(image_gallery)
       expect(post.bootsy_image_gallery_id).to eq(image_gallery.id)
+    end
+
+    it 'does not set an image gallery if container already has a gallery' do
+      post_with_gallery.bootsy_image_gallery_id = image_gallery.id
+
+      expect(post_with_gallery.bootsy_image_gallery_id).not_to eq(image_gallery.id)
     end
   end
 end
