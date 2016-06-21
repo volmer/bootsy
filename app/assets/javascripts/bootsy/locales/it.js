c20c0f693862863d470a6aa4d23fc012a0b576b9
Andrea Dal Ponte <info@andreadalponte.com>
2016-06-21 13:12:04 +0200

    Change: add italian locale

---
+++ app/assets/javascripts/bootsy/locales/it.js
@@ -0,0 +1,54 @@
+/**
+ * Italian translations for bootstrap-wysihtml5 and Bootsy
+ */
+(function($){
+  $.fn.wysihtml5.locale['it'] = {
+    font_styles: {
+      normal: 'Testo normale',
+      h1: 'Titolo 1',
+      h2: 'Titolo 2',
+      h3: 'Titolo 3'
+    },
+    emphasis: {
+      bold: 'Grassetto',
+      italic: 'Corsivo',
+      underline: 'Sottolineato',
+      small: 'Piccolo'
+    },
+    lists: {
+      unordered: "Lista non ordinata",
+      ordered: "Lista ordinata",
+      outdent: "Elimina rientro",
+      indent: "Aggiungi rientro"
+    },
+    link: {
+      insert: 'Inserisci link',
+      cancel: 'Annulla',
+      target: 'Apri in una nuova finestra'
+    },
+    image: {
+      insert: 'Inserisci immagine',
+      cancel: 'Annulla'
+    },
+    html: {
+      edit: 'Modifica HTML'
+    },
+    colours: {
+      black: "Nero",
+      silver: "Argento",
+      gray: "Grigio",
+      maroon: "Marrone",
+      red: "Rosso",
+      purple: "Viola",
+      green: "Verde",
+      olive: "Oliva",
+      navy: "Blu Marino",
+      blue: "Blu",
+      orange: "Arancio"
+    },
+    bootsy: {
+      alertUnsaved: 'Hai delle modifiche non salvate.',
+      error: 'Qualcosa è andato storto. Per favore riprova più tardi.'
+    }
+  };
+}(jQuery));
